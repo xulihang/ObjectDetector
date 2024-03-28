2c8517ab8beb40e52e054dc5f9026da2302a7e82
xulihang <xulihanghai@163.com>
2024-03-28 10:04:33 +0800

    use Test.java to draw the bounding boxes

--- src/org/xulihang/Test.java
+++ src/org/xulihang/Test.java
@@ -1,6 +1,12 @@
 package org.xulihang;
 
-import java.io.IOException;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Rect2d;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class Test {
 
@@ -10,9 +16,15 @@ public class Test {
         ONNXDetector detector = new ONNXDetector("G://yolo//yolov8n.onnx");
         //ObjectDetector detector = new ObjectDetector("G://yolo//yolov8n.onnx",640,640);
         try {
-			var results = detector.Detect("G://git//object-detection-and-barcode-reading//IMG20240326163255.jpg");
+        	Mat img = Imgcodecs.imread("G://git//object-detection-and-barcode-reading//IMG20240326163255.jpg");
+			var results = detector.Detect(img);
 			System.out.println(results);
-		} catch (IOException e) {
+			System.out.println(results.size());
+			for (Rect2d rect:results) {
+				Imgproc.rectangle(img, new Rect((int)rect.x,(int)rect.y,(int)rect.width,(int)rect.height), new Scalar(255,0,0),5);
+			}
+			Imgcodecs.imwrite("G://git//object-detection-and-barcode-reading//out.jpg",img);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
